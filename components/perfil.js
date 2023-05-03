c3efcff4da616e35298a610ef146f54c4eb85aa6
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2023-05-03 00:54:54 -0300

    learning styles and tag components

--- components/perfil.js
+++ components/perfil.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 
-export default function Perfil() {
-
+export const Perfil = () => {
     return (
         <>
         <Image 
--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -1,12 +1,24 @@
+import Head from "next/head"
 import Link from "next/link"
+import Script from "next/script"
+import Layout from "../../components/layout"
 
 export default function FirstPost() {
     return (
-    <>
-    <h1>First Post</h1>
-    <h2>
-        <Link href="/">Back to home</Link>
-    </h2>
-    </>
+    <Layout>
+        <Head>
+            <h1>First Post</h1>
+        </Head>
+        <Script 
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() => {
+                console.log('Script loaded correctly')
+            }}
+        />
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
+    </ Layout>
     )
 }
