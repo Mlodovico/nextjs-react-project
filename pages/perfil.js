1286b8e1f0ca88e8a282ce9f5006c976c2aa488d
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2023-05-02 23:52:05 -0300

    next images

---
+++ pages/perfil.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Perfil() {
+
+    return (
+        <>
+        <Image 
+        src='../public/images/riding_img.jpg'
+        alt="Riding"
+        height={350}
+        width={500}
+        />
+        </>
+    )
+
+}
